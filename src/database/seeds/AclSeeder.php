462dbc49921382f75bbe0b71a234945ac9b5b404
stygiansabyss <stygian.warlock.v2@gmail.com>
2016-02-21 14:51:30 -0600

    Fixing seeder

--- src/database/seeds/AclSeeder.php
+++ src/database/seeds/AclSeeder.php
@@ -15,13 +15,13 @@ class AclSeeder extends Seeder {
     {
         // Truncate the table each time.
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('permissions')->truncate();
+        DB::table('acl_permissions')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         $permissions = config('nukacode-user.permissions');
 
         // Add any data to the table.
-        DB::table('permissions')->insert($permissions);
+        DB::table('acl_permissions')->insert($permissions);
     }
 
     private function insertRoles()
@@ -29,26 +29,26 @@ class AclSeeder extends Seeder {
 
         // Uncomment the below to wipe the table clean before populating
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('roles')->truncate();
+        DB::table('acl_roles')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         // These are set in the config so the individual site can override the defaults when desired.
         $roles = config('nukacode-user.roles');
 
         // Uncomment the below to run the seeder
-        DB::table('roles')->insert($roles);
+        DB::table('acl_roles')->insert($roles);
     }
 
     private function linkRolesAndPermissions()
     {
         // Truncate the table each time.
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('permission_role')->truncate();
+        DB::table('acl_permission_role')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         $permissions = config('nukacode-user.permission_role');
 
         // Add any data to the table.
-        DB::table('permission_role')->insert($permissions);
+        DB::table('acl_permission_role')->insert($permissions);
     }
 }
